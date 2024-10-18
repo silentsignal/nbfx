57cddb3a9c0f52f5d0cf439ec99ec69d85f8ceed
b <vpbalint@silentsignal.hu>
2024-10-18 20:19:49 +0200

    updating string lengths

--- src/nbfx/__init__.py
+++ src/nbfx/__init__.py
@@ -12,9 +12,9 @@ def nbfx_export_values(nbfx: Nbfx) -> list:
             rec = record.rec_body
             if isinstance(rec, Nbfx.PrefixAttribute):
                 ret.append((i, "name", rec.name.str))
-                ret.append((i, "value", rec.name.str))
+                ret.append((i, "value", rec.value.str))
             elif isinstance(rec, Nbfx.DictionaryAttribute):
-                ret.append((i, "prefix", rec.name.str))
+                ret.append((i, "prefix", rec.prefix.str))
         except AttributeError:
             pass
     return ret
@@ -24,11 +24,12 @@ def nbfx_import_values(nbfx: Nbfx, values) -> Nbfx:
     for val in values:
         rec = nbfx.records[val[0]].rec_body
         nbfx_str = getattr(rec, val[1])
-        nbfx_str.str = val[2]
+        nbfx_set_string(nbfx_str, val[2])
     return nbfx
 
 
 def nbfx_serialize(nbfx: Nbfx) -> bytes:
+    nbfx._check()
     final_size = 0
     try:
         _test_io = KaitaiStream(BytesIO(bytearray(1024)))
@@ -39,3 +40,13 @@ def nbfx_serialize(nbfx: Nbfx) -> bytes:
     _out_io = KaitaiStream(BytesIO(bytearray(final_size)))
     nbfx._write(_out_io)
     return _out_io.to_byte_array()
+
+def nbfx_set_string(nbfx_str: Nbfx.NbfxString, value: str):
+    nbfx_str.str = value
+    nbfx_set_multibyte_int31(nbfx_str.str_len, len(value))
+
+def nbfx_set_multibyte_int31(nbfx_int: Nbfx.MultiByteInt31, value: int):
+    if value <= 0x7f:
+        nbfx_int.multibytes[0].value = value
+        nbfx_int.multibytes[0].has_next = 0
+        return
