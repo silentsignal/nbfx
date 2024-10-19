7b2bbc8b03dcd5726fcbc5983aac17fe847c5f74
b <vpbalint@silentsignal.hu>
2024-10-19 12:32:13 +0200

    file reader fixture for pytest

--- tests/test_nbfx.py
+++ tests/test_nbfx.py
@@ -12,16 +12,19 @@ sample_files = [
     join("samples", f) for f in listdir("samples") if isfile(join("samples", f))
 ]
 
-
-@pytest.mark.parametrize("file", sample_files)
-def test_reserialize_str_len_2bytes(file):
+@pytest.fixture()
+def nbfx_from_file(file):
     with KaitaiStream(open(file, "rb")) as _io:
         nbfx = Nbfx(_io)
         nbfx._read()
-        vals = nbfx_export_values(nbfx)
-        new_vals = []
-        for val in vals:
-            new_vals.append((val[0], val[1], "A" * 5521))
-        nbfx_import_values(nbfx, new_vals)
-        result = nbfx_serialize(nbfx)
-        assert b"\x91+AAAA" in result
+        yield nbfx
+
+@pytest.mark.parametrize("file", sample_files)
+def test_reserialize_str_len_2bytes(nbfx_from_file):
+    vals = nbfx_export_values(nbfx_from_file)
+    new_vals = []
+    for val in vals:
+        new_vals.append((val[0], val[1], "A" * 5521))
+    nbfx_import_values(nbfx_from_file, new_vals)
+    result = nbfx_serialize(nbfx_from_file)
+    assert b"\x91+AAAA" in result
